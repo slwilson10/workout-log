c2a59188a9cc02764b25294647f548b9625657b6
slwilson10 <slwilson10@gmail.com>
2016-03-30 19:06:11 +0000

    Update index, list, sidebar htmls for minor styling Add header.html Update sidebar.html, views.py, urls.py to accept year value Update views.py to split 'list' def into 'all' and 'month' Todo:     * Update css for minor styling     * Add charts     * Add indv view     * Add icons for delete and update

--- workout/views.py
+++ workout/views.py
@@ -23,13 +23,52 @@ class WorkoutForm(ModelForm):
             'seconds':TextInput(attrs={'size':1}),
             'distance':TextInput(attrs={'size':2}),
         }
+def all(request):
+    workouts = Workout.objects.all()
+    month = 'All'
+    workout_dates = get_dates()
+    form = WorkoutForm()
+    WorkoutFormset = modelformset_factory(Workout, form=WorkoutForm,
+                         can_delete=True, can_order=True, extra=0)
+    formset = WorkoutFormset(queryset=workouts.order_by('-date'))   
+    context = {'workouts': workouts, 'formset': formset,
+            'workout_dates': workout_dates, 'month': month}
+    return render(request, 'list.html', context)
 
-def list(request, month):
-    if month == 'All':    
-        workouts = Workout.objects.all()
-    else:
-        month = datetime.strptime(month,'%b').month
-        workouts = Workout.objects.filter(date__month=month)
+
+def month(request, year,  month):
+    year = datetime.strptime(year, '%Y').year
+    month = datetime.strptime(month,'%b').month
+    workouts = Workout.objects.filter(date__year=year, date__month=month)
+    workout_dates = get_dates()
+    form = WorkoutForm()
+    WorkoutFormset = modelformset_factory(Workout, form=WorkoutForm,
+                         can_delete=True, can_order=True, extra=0)
+    formset = WorkoutFormset(queryset=workouts.order_by('-date'))   
+    context = {'workouts': workouts, 'formset': formset,
+            'workout_dates': workout_dates, 'month': month}
+    return render(request, 'list.html', context)
+	
+def delete(request, year, month, pk):
+    workout = get_object_or_404(Workout, pk=pk)
+    if request.method == 'DELETE':
+        workout.delete()
+        data = {'Delete':'ok'}
+        return HttpResponse(json.dumps(data), content_type='application/json')
+    return redirect('list/'+year+'/'+month)
+
+def update(request, month):
+    WorkoutFormset = modelformset_factory(Workout, form=WorkoutForm)
+    if request.method == 'POST':
+        formset = WorkoutFormset(request.POST)
+        if formset.is_valid():
+            print ('POSTED')
+            formset.save()
+        return redirect('/list/'+year+'/'+month)
+    else: print('NOT VALID')
+    return redirect('/list/'+year+'/'+month)
+
+def get_dates():
     workout_dates = []
     years = []
     all_workouts = Workout.objects.all()
@@ -39,32 +78,7 @@ def list(request, month):
         date = {'year':y,'months':m}
         if date not in workout_dates:
             workout_dates.append(date)
-    form = WorkoutForm()
-    WorkoutFormset = modelformset_factory(Workout, form=WorkoutForm,
-                         can_delete=True, can_order=True, extra=0)
-    formset = WorkoutFormset(queryset=workouts.order_by('-date'))   
-    context = {'workouts': workouts, 'formset': formset,
-            'workout_dates': workout_dates, 'month': month}
-    return render(request, 'list.html', context)
-	
-def delete(request, month, pk):
-    workout = get_object_or_404(Workout, pk=pk)
-    if request.method == 'DELETE':
-        workout.delete()
-        data = {'Delete':'ok'}
-        return HttpResponse(json.dumps(data), content_type='application/json')
-    return redirect('list' + month)
-
-def update(request, month):
-    WorkoutFormset = modelformset_factory(Workout, form=WorkoutForm)
-    if request.method == 'POST':
-        formset = WorkoutFormset(request.POST)
-        if formset.is_valid():
-            print ('POSTED')
-            formset.save()
-        return redirect('/list/'+ month)
-    else: print('NOT VALID')
-    return redirect('/list/'+ month)
+    return workout_dates
 
 def get_months(year):
     workouts = Workout.objects.filter(date__year=year)
--- workout_site/urls.py
+++ workout_site/urls.py
@@ -3,8 +3,9 @@ from django.contrib import admin
 import workout.views
 
 urlpatterns = [
-        url(r'^list/(?P<month>\w+)$', workout.views.list),
-        url(r'^delete/(?P<month>\w+)/(?P<pk>\d+)$', workout.views.delete),
-	url(r'^update/(?P<month>\w+)$', workout.views.update),
+        url(r'^list/All/$', workout.views.all),
+        url(r'^list/(?P<year>\d+)/(?P<month>\w+)$', workout.views.month),
+        url(r'^delete/(?P<year>\d+)/(?P<month>\w+)/(?P<pk>\d+)$', workout.views.delete),
+	url(r'^update/(?P<year>\d+)/(?P<month>\w+)$', workout.views.update),
     	url(r'^admin/', include(admin.site.urls)),
 ]
