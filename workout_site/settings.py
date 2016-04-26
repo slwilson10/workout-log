5015251e660ee692787f3274e7be9daea2199ca3
slwilson10 <slwilson10@gmail.com>
2016-04-26 17:59:02 +0000

    Debugging

--- workout_site/settings.py
+++ workout_site/settings.py
@@ -96,7 +96,12 @@ WSGI_APPLICATION = 'workout_site.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-DATABASES['default'] = dj_database_url.config()
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
