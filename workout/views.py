c551f267af78f4959a13ac6a73ca8492538711f3
slwilson10 <slwilson10@gmail.com>
2016-05-26 19:36:37 +0000

    Update for major UI and code change Update view.py & urls.py to get and show date range Add footer.html Add main.html to serve as main page containing charts and workout history Removed unnecessary code from workout.css & workout.js

--- workout/views.py
+++ workout/views.py
@@ -4,7 +4,9 @@ from django.forms import ModelForm
 from django.forms import TextInput
 from django.forms import modelformset_factory
 import json
-from datetime import datetime
+from datetime import date
+from datetime import timedelta
+from dateutil.relativedelta import relativedelta
 from .models import Workout
 
 ## Class for model workout form
@@ -24,33 +26,21 @@ class WorkoutForm(ModelForm):
             'seconds':TextInput(attrs={'style':'width:1em'}),
             'distance':TextInput(attrs={'size':2}),
         }
-
-## Show year view
-def year_view(request):
-    years = get_years()
-    context = {'years': years}
-    return render(request, 'year.html', context)
-
-## Show month view
-def month_view(request, year): 
-    months = get_months(year)
-    context = {'months': months, 'year': year}
-    return render(request, 'month.html', context)
-
 ## Show workout view
-def workout_view(request, year,  month):
-    ## Get month number for model filter
-    m = datetime.strptime(month,'%b').month
-    workouts = Workout.objects.filter(date__year=year, date__month=m)
-    workout_dates = get_dates()
+cur_date = date.today()
+past_date = cur_date+relativedelta(months=-1)
+var_date_range= (cur_date,past_date)
+def main(request, date_range=var_date_range):
+    dates_range = {'cur':date_range[0],'past':date_range[1]}
+    workouts = Workout.objects.filter(date__range=[date_range[0],date_range[1]])
     ## Create formset from form class
     form = WorkoutForm()
     WorkoutFormset = modelformset_factory(Workout, form=WorkoutForm,
                          can_delete=True, can_order=True, extra=0)
     formset = WorkoutFormset(queryset=workouts.order_by('-date'))   
-    context = {'workouts': workouts, 'formset': formset, 
-                'year':year, 'month': month}
-    return render(request, 'workout.html', context)
+    context = {'workouts': workouts, 'formset': formset,
+                'date_range':dates_range}
+    return render(request, 'main.html', context)
 
 ## Delete workout
 def delete(request, year, month, pk):
@@ -73,46 +63,3 @@ def update(request, year, month):
     else: print('NOT VALID')
     return redirect('/list/'+year+'/'+month)
 
-
-def get_dates():
-    workout_dates = []
-    years = []
-    all_workouts = Workout.objects.all()
-    for w in all_workouts:
-        y = w.date.year
-        m = get_months(y)
-        date = {'year':y,'months':m}
-        if date not in workout_dates:
-            workout_dates.append(date)
-    return workout_dates
-
-## Get years containing workouts
-def get_years():
-    years_group = []
-    years = []
-    for w in Workout.objects.all():
-        year = w.date.year
-        if year not in years:
-            years.append(year)    
-    for y in years:
-        workouts = len(Workout.objects.filter(date__year=y))
-        group = {'year': y,'workouts': workouts}
-        years_group.append(group)
-    return years_group
-
-## Get month with given year containing workouts
-def get_months(year):
-    workouts = Workout.objects.filter(date__year=year)
-    months_group = []
-    months = []
-    for w in workouts:
-        month = w.date.strftime('%b')
-        if month not in months:
-            months.append(month)
-    for m in months:
-        month=datetime.strptime(m,'%b').month
-        workouts = len(Workout.objects.filter(date__year=year,
-                        date__month=month))
-        group = {'month':m,'workouts':workouts}
-        months_group.append(group)
-    return months_group
--- workout_site/urls.py
+++ workout_site/urls.py
@@ -3,9 +3,8 @@ from django.contrib import admin
 import workout.views
 
 urlpatterns = [
-        url(r'^$', workout.views.year_view),
-        url(r'^(?P<year>\d+)/$', workout.views.month_view),
-        url(r'^(?P<year>\d+)/(?P<month>\w+)/$', workout.views.workout_view),
+        url(r'^$', workout.views.main),
+        url(r'^(?P<date_range>\w+)/$', workout.views.main),
         url(r'^(?P<year>\d+)/(?P<month>\w+)/(?P<pk>\d+)/delete/$', workout.views.delete),
 	url(r'^(?P<year>\d+)/(?P<month>\w+)/update/$', workout.views.update),
     	url(r'^admin/', include(admin.site.urls)),
